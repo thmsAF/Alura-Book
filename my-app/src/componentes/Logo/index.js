53814e9bac4a55e4d267d4230b329acbb4a3c242
Thomas <almeidaf.thomas@gmail.com>
2023-02-28 21:13:14 -0300

    primeiro commit

---
+++ my-app/src/componentes/Logo/index.js
@@ -0,0 +1,11 @@
+import logo from "../../Imagens/logo.svg";
+
+function Logo (){
+    return (
+        <div className='logo'>
+            <img src={logo} alt='logo'></img>
+            <p><strong>Alura</strong>Books</p>
+        </div>
+    )
+}
+export default Logo
